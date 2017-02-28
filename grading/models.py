cf9bc110a4dc83bbd9d9bd8b2b4da97a9e98b8b9
David Fokkema <davidfokkema@gmail.com>
2017-02-28 17:11:06 +0100

    First stab at models for grading app

--- grading/models.py
+++ grading/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    user = models.CharField()
+    password = models.CharField()
+    account_type = models.CharField(choices=[('blackboard', 'blackboard')])
+
+class Course(models.Model):
+    title = models.CharField()
+    course_id = models.CharField()
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+class Student(models.Model):
+    first_name = models.CharField()
+    prefix = models.CharField()
+    last_name = models.CharField()
+    course = models.ManyToManyField(Course)
