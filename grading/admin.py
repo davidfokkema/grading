3865f4cc58685d59ae73d7775ffcadb94fc0dfbb
David Fokkema <davidfokkema@gmail.com>
2017-02-28 22:14:37 +0100

    Enable admin interface for models

--- grading/admin.py
+++ grading/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account, Course, Student
+
+admin.site.register(Account)
+admin.site.register(Course)
+admin.site.register(Student)
