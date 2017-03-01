82e7473e28b76ceee9b5006a22af8944241f5a6c
David Fokkema <davidfokkema@gmail.com>
2017-03-01 15:09:58 +0100

    First stab at course list view

--- grading/views.py
+++ grading/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Course, Account
+
+
+class IndexView(generic.ListView):
+    model = Course
+    template_name = 'grading/index.html'
