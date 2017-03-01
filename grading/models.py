a7bb8e1becc9979973cb2dc72fef2a4ae4b94130
David Fokkema <davidfokkema@gmail.com>
2017-03-01 14:04:13 +0100

    Better account choices and string

--- grading/models.py
+++ grading/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 class Account(models.Model):
+
+    ACCOUNT_CHOICES = [('bb_uva', 'Blackboard UvA'),
+                       ('bb_vu', 'Blackboard VU')]
+
     user = models.CharField(max_length=20)
     unsafe_password = models.CharField(max_length=80)
-    account_type = models.CharField(max_length=20,
-                                    choices=[('blackboard', 'blackboard')])
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_CHOICES)
+
+    def __str__(self):
+        return '%s - %s' % (self.get_account_type_display(), self.user)
+
 
 class Course(models.Model):
     title = models.CharField(max_length=20)
