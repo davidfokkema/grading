74713e06069154d771b7dabb81f31473056890eb
David Fokkema <davidfokkema@gmail.com>
2017-04-08 21:51:39 +0200

    Make mails per assignment optional

--- grading/models.py
+++ grading/models.py
@@ -39,8 +39,8 @@ class Assignment(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     assignment_type = models.CharField(max_length=20,
                                        choices=ASSIGNMENT_CHOICES)
-    mail_subject = models.CharField(max_length=100)
-    mail_body = models.TextField()
+    mail_subject = models.CharField(max_length=100, blank=True, null=True)
+    mail_body = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return '%s (%s) - %s' % (self.title,
